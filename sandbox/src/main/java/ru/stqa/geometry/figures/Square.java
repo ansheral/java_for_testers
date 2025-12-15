8df5e1f9d1b2dd4fe904f50f9dd8b99d4e12e473
Anastasiia <anastasiiashershneva@gmail.com>
2025-12-15 19:19:06 +0300

    Написаны тесты для площади и периметра квадрата

--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -2,10 +2,14 @@ package ru.stqa.geometry.figures;
 
 public class Square {
     public static void printSquareArea(double side){
-        System.out.println("Площадь квадрата со стороной " + side + " равняется " + squareArea(side));
+        System.out.println("Площадь квадрата со стороной " + side + " равняется " + area(side));
     }
 
-    private static double squareArea(double a) {
+    public static double area(double a) {
         return a * a;
+    }
+
+    public static double perimeter(double a) {
+        return 4 * a;
     }
 }
